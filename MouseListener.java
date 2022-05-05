d0bf655635a9a95e6e8d52665bdfc9a6cd90f013
punmilan <94536637+punmilan@users.noreply.github.com>
2022-05-05 19:22:27 +0545

    Add files via upload

---
+++ MouseListener.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class MouseListener extends JFrame implements java.awt.event.MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Graphics g = getGraphics();
+        g.setColor(Color.red);
+        g.drawRect(e.getX(),e.getY(),100,100);
+        g.setFont(new Font(Font.SERIF,Font.BOLD,45));
+        g.drawString("Hello World",e.getX(),e.getY());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new MouseListener();
+
+    }
+
+     public MouseListener(){
+        setTitle("My mouse listener");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setSize(500,500);
+        addMouseListener(this);
+     }
+}
+
